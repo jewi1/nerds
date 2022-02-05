7fabeeebbdfed91277699778f8feaa8e9b740c47
Vyacheslav <slava.89989@gmail.com>
2022-02-05 13:25:04 +0500

    Финальная версия с анимациями и скриптами

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,33 @@ import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+let writeUsBtn = document.querySelector(".map__write-us");
+let popup = document.querySelector(".popup");
+let popupClose = popup.querySelector(".popup__cross");
+let sliders = document.querySelectorAll(".slider__slide");
+let sliderBtns = document.querySelectorAll(".slider__btn");
+let sliderCotainer = document.querySelector(".slider .container");
 
-const swiper = new Swiper();
+let activeSlide = 0;
+
+writeUsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  popup.classList.add("popup--active")
+})
+
+popupClose.addEventListener("click", (e) => {
+  popup.classList.remove("popup--active")
+})
+
+for (let i = 0; i < sliderBtns.length; i++) {
+  sliderBtns[i].addEventListener("click", (e) => {
+    e.preventDefault()
+    sliders[activeSlide].classList.remove("slider__slide--active")
+    sliderBtns[activeSlide].classList.remove("slider__btn--active")
+    sliderCotainer.classList.remove(`container--slide-${activeSlide+1}`)
+    activeSlide = i
+    sliders[activeSlide].classList.add("slider__slide--active")
+    sliderBtns[activeSlide].classList.add("slider__btn--active")
+    sliderCotainer.classList.add(`container--slide-${activeSlide+1}`)
+  })
+}
